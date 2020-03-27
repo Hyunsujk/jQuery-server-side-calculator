9a3a02aa2a4045a0d7deddbab45e17b57e1fdb1c
Hyunsujk <hyunsujk@gmail.com>
2020-03-27 15:42:17 -0500

    crating a numberhistory to return all history

--- server/server.js
+++ server/server.js
@@ -4,8 +4,16 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const numberHistory = require("./modules/number_history");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(express.static("server/public"));
+
+app.get("/numberHistory", (req, res) => {
+  res.send(numberHistory);
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
