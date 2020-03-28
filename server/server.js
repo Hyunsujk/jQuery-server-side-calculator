d07d91d5747765e0099e75a52a109994780f5868
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-03-28 14:21:08 -0500

    save the number input and does calculation, now I need to figure out how to save the calculation in the object

--- server/server.js
+++ server/server.js
@@ -16,10 +16,17 @@ let num2;
 let numOutput;
 
 app.get("/numberHistory", (req, res) => {
+  res.send(numberHistory);
+});
+
+app.post("/numberHistory", (req, res) => {
+  const numbers = req.body;
+  numberHistory.push(numbers);
+  console.log(numbers);
   for (let i = 0; i < numberHistory.length; i++) {
     operator = numberHistory[i].operation_sign;
-    num1 = numberHistory[i].number_one;
-    num2 = numberHistory[i].number_two;
+    num1 = parseFloat(numberHistory[i].number_one);
+    num2 = parseFloat(numberHistory[i].number_two);
     numOutput = numberHistory[i].number_output;
     console.log(operator, num1, num2);
     if (operator == "+") {
@@ -33,45 +40,6 @@ app.get("/numberHistory", (req, res) => {
     }
     console.log(numOutput);
   }
-  res.send(numberHistory);
-});
-
-app.post("/numberHistory", (req, res) => {
-  const numbers = req.body;
-  numberHistory.push(numbers);
-  // console.log(numbers);
-  // for (let i = 0; i < numberHistory.length; i++) {
-  //   operator = numberHistory[i].operation_sign;
-  //   num1 = numberHistory[i].number_one;
-  //   num2 = numberHistory[i].number_two;
-  //   numOutput = numberHistory[i].number_output;
-  //   console.log(operator, num1, num2);
-  //   if (operator == "+") {
-  //     numOutput = num1 + num2;
-  //   } else if (operator == "-") {
-  //     numOutput = num1 - num2;
-  //   } else if (operator == "*") {
-  //     numOutput = num1 * num2;
-  //   } else if (operator == "/") {
-  //     numOutput = num1 / num2;
-  //   }
-  //   console.log(numOutput);
-  // }
-  // for (let i = 0; i < numberHistory.length; i++) {
-  //   if (numberHistory[i].operation_sign == "+") {
-  //     numberHistory[i].number_output =
-  //       numberHistory[i].number_one + numberHistory[i].number_two;
-  //   } else if (numberHistory[i].operation_sign == "-") {
-  //     numberHistory[i].number_output =
-  //       numberHistory[i].number_one - numberHistory[i].number_two;
-  //   } else if (numberHistory[i].operation_sign == "*") {
-  //     numberHistory[i].number_output =
-  //       numberHistory[i].number_one * numberHistory[i].number_two;
-  //   } else if (numbers.operation_sign == "/") {
-  //     numberHistory[i].number_output =
-  //       numberHistory[i].number_one / numberHistory[i].number_two;
-  //   }
-  // }
 
   res.sendStatus(201);
 });
