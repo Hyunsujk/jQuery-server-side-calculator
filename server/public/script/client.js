0127fd3faf5f773d23a28131b02e1a7562ff98eb
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-03-29 16:24:35 -0500

    added explanation comments to code

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -4,16 +4,21 @@ let operator;
 $(document).ready(init);
 
 function init() {
+  //get history of the calculation
   getHistory();
+  // when the operator button is clicked, store the operation to the variable operator
   $(".js-btn-operator").on("click", function() {
     operator = event.target.innerHTML;
   });
+  // when equals button is clicked, get input values from input field
   $(".js-btn-equal").on("click", getInput);
+  // when clear button is clicked, empty the input field
   $(".js-btn-clear").on("click", emptyInputField);
 
   //
   // stretch goal
   //---------------------------------
+  //when stretch goal calculator button is clicked, do the calculation
   $(".js-btn-stretch").on("click", stretchGoalCalculator);
 }
 
@@ -22,17 +27,19 @@ function init() {
 //---------------
 
 function getInput() {
+  // store the input values and operator in the numberInput object
   const numberInput = {
     number_one: parseInt($(".js-number-input-1").val()),
     number_two: parseInt($(".js-number-input-2").val()),
     operation_sign: operator,
     number_output: ""
   };
-
+  // save the numberInput
   saveInput(numberInput);
 }
 
 function emptyInputField() {
+  // empty the input field
   $(".js-number-input-1").val("");
   $(".js-number-input-2").val("");
   $(".js-number-output").text(`0`);
@@ -43,32 +50,38 @@ function emptyInputField() {
 //------------------------------
 
 function saveInput(input) {
+  // post the input value to the server using "/numberhistory" route
   $.ajax({
     method: "POST",
     url: "/numberhistory",
     data: input
   })
     .then(response => {
+      // then get the updated calculation history
       console.log(response);
       getHistory();
     })
     .catch(err => {
+      // if there is an error, let user know
       console.warn("Oops! couldn't save the output");
     });
 }
 
 function getHistory() {
   console.log("in get history");
+  // get the calculation history through "/numberhisotry" route
   $.ajax({
     type: "GET",
     url: "/numberhistory"
   })
     .then(response => {
+      // store the response in the history then render the calculation history to the DOM
       history = response;
       console.log(history);
       render();
     })
     .catch(err => {
+      // if there is an error, let user know
       console.warn("Oops! something went wrong");
     });
 }
@@ -79,7 +92,9 @@ function getHistory() {
 
 function render() {
   console.log("in render");
+  // empty the history DOM
   $(".js-number-history").empty();
+  // loop through each index in history array, and append each of them in unlisted order format
   for (let i = 0; i < history.length; i++) {
     const result = history[i];
     console.log(result);
@@ -87,6 +102,7 @@ function render() {
     <li>${result.number_one}${result.operation_sign}${result.number_two}=${result.number_output}</li>
     `);
   }
+  // if last history is not undefined, display the number output of last index in the history to DOM
   const last_history = history[history.length - 1];
   if (last_history != undefined) {
     $(".js-number-output").text(`
@@ -104,45 +120,55 @@ let currentEntry = "0";
 let operation = null;
 
 function stretchGoalCalculator(event) {
+  // btnPressed stores the value of the button pressed
   let btnPressed = event.target.innerHTML;
   console.log(btnPressed);
 
   if (btnPressed == "C") {
+    //if the btnPressed is C, result and currentEntry are set to 0
     result = 0;
     currentEntry = "0";
   } else if (btnPressed == ".") {
+    // if btnPressed is "." add to the currentEntry
     currentEntry += ".";
   } else if (isNumber(btnPressed)) {
+    // if btnPressed is number and currentEntry is 0, btnPressed is the new currentEntry
     if (currentEntry == "0") {
       currentEntry = btnPressed;
     } else {
+      // else, currentEntry is updated to currentEntry concatenated with the btnPressed value
       currentEntry = currentEntry + btnPressed;
     }
   } else if (isOperator(btnPressed)) {
+    // if btnPress is an operator, store the currentEntry as prevEntry, and set operation to the pressed operator
+    // then set currentEntry as an empty string
     prevEntry = parseFloat(currentEntry);
     operation = btnPressed;
     currentEntry = "";
   } else if (btnPressed == "=") {
+    // if the button pressed is "=", do the operate function with the values, and set the currentEntry with the calculated value
+    // then set the operation as null
     currentEntry = operate(prevEntry, currentEntry, operation);
     operation = null;
   }
-
+  // update the screen with currentEntry
   updateScreen(currentEntry);
 }
 
 function updateScreen(displayValue) {
+  //display value is a string of number, and display up to 10 digits in the screen
   displayValue = displayValue.toString();
   $(".screen").html(displayValue.substring(0, 10));
 }
-
+// if the value is not 'Not a Number', return the value
 function isNumber(value) {
   return !isNaN(value);
 }
-
+// if the value matches with one of the four operators, return the operator
 function isOperator(value) {
   return value == "/" || value == "*" || value == "+" || value == "-";
 }
-
+// do the calculation according to the operation
 function operate(a, b, operation) {
   a = parseFloat(a);
   b = parseFloat(b);
--- server/server.js
+++ server/server.js
@@ -1,18 +1,25 @@
+// call express and body-parser from package.json to use
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+// set the port as 5000
 const PORT = process.env.PORT || 5000;
-
+// numberHistory get the value from number_history module
 const numberHistory = require("./modules/number_history");
-
+// sent data will be stored in body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("server/public"));
-
+// get the number history through "/numberhistory" route
 app.get("/numberhistory", (req, res) => {
   res.send(numberHistory);
 });
+
+// when the number input is sent from client.js, do the calculation with the operator
+// and update the value of the calculated number
+// then push the updated information to numberHistory module.
+// send status code 201 to confirm the new input is created.
 
 app.post("/numberhistory", (req, res) => {
   const numbers = req.body;
@@ -35,6 +42,7 @@ app.post("/numberhistory", (req, res) => {
   res.sendStatus(201);
 });
 
+// the application listen through port 5000
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
